891d8e7967e3bf2198579b7052d26840eb28dea7
jxd <xdjiaxd@gmail.com>
2019-02-07 19:41:02 +0800

    prevent ctrl u conflict with vim

--- config.h
+++ config.h
@@ -59,8 +59,8 @@ static Key keys[] = {
 
 	{ MODKEY,               XK_q,      killclient,  { 0 } },
 
-	{ MODKEY,               XK_u,      focusurgent, { 0 } },
-	{ MODKEY|ShiftMask,     XK_u,      toggle,      { .v = (void*) &urgentswitch } },
+	{ MODKEY,               XK_t,      focusurgent, { 0 } },
+	{ MODKEY|ShiftMask,     XK_t,      toggle,      { .v = (void*) &urgentswitch } },
 
 	{ 0,                    XK_F11,    fullscreen,  { 0 } },
 };
